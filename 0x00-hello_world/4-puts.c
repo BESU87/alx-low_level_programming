6f4454f60b85013fa96378b265a7dc437f44b3b8
BESU87 <kukucoss@gmail.com>
2022-12-12 09:53:35 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+/*
+ * main -Entry Point
+ *
+ * Retrun: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
+}
