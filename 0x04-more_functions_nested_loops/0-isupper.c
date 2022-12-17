956b21e84741e7783ba27f5b94042470187e0164
BESU87 <kukucoss@gmail.com>
2022-12-17 10:51:03 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,12 @@
 /**
  * _isupper - checks for uppercase character
  * @c: Variable text
- * Return: Always 0.
+ * Return: Always 0 or 1
  */
 int _isupper(int c)
 {
-	return (1);
-}
-else
-{
-	return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
