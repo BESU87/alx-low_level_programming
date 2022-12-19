9f0b53d8176451ccbe5d858df48376aa434c33e6
BESU87 <kukucoss@gmail.com>
2022-12-19 09:21:11 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - function that takes a pointer to an integer parameter
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
